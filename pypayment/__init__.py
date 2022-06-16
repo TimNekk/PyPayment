04696c36a8b55438293833d7671cffe594d54522
Tim <herew26@gmail.com>
2022-06-17 00:26:05 +0300

    file yoomoney renamed

--- pypayment/__init__.py
+++ pypayment/__init__.py
@@ -2,4 +2,4 @@ from .exceptions import NotAuthorized, PaymentCreationError, PaymentGettingError
 from .status import PaymentStatus
 from .payment import Payment
 from .qiwi import QiwiPayment
-from .yoomoneypayment import YooMoneyPayment, YooMoneyPaymentType
+from .yoomoney import YooMoneyPayment, YooMoneyPaymentType
