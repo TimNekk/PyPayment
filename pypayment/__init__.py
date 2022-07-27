058eab0b9b02073259d65cbfa4174c0ae605f60b
Timofey Kochetov <herew26@gmail.com>
2022-07-27 04:01:45 +0300

    (v1.3.0)

--- pypayment/__init__.py
+++ pypayment/__init__.py
@@ -1,6 +1,6 @@
 from .exceptions import NotAuthorized, PaymentCreationError, PaymentGettingError, AuthorizationError
-from .status import PaymentStatus
 from .commission import ChargeCommission
+from .status import PaymentStatus
 from .payment import Payment
 from .qiwi import QiwiPayment, QiwiPaymentType
 from .yoomoney import YooMoneyPayment, YooMoneyPaymentType
